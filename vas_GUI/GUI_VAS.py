a8c785275917bfd1795fe4c6515f187a538fc8ed
VarunSatyadevShetty <vashetty@umich.edu>
2024-05-14 11:03:59 -0400

    Added dynamically changing the button and slider position, fixed slider<-> button mapping -- {working but still needs a bit of stylistic cleanup}

--- vas_GUI/GUI_VAS.py
+++ vas_GUI/GUI_VAS.py
@@ -17,6 +17,7 @@ from kivy.properties import NumericProperty
 import numpy as np
 import time
 import csv
+from functools import partial
 
 import Message_pb2
 import Message_pb2_grpc
@@ -42,19 +43,20 @@ class GuiVas(BoxLayout):
 
         # Ask user for name of csv file and start the logger
         print("Filename to save as (format:Subject_VAS_pres#_inclinelvl).csv => ") 
-        self.filename = input()
-        self.headers = ['Time(s)', 'Current Torque Experienced', 'Torque Slider Adjusted', 'VAS Value of Torque Slider', 'Confirm Button Pressed']
+        self.filename = '1'#input()
+        self.headers = ['Time(s)', 'Current Torque Experienced', 'Torque Slider Adjusted', 'VAS Value of Torque Slider', 'Confirm Button Pressed']#, 'Slider_Value_Changed', 'Torque_Button_Pressed']
         self.logged_yet = False
         self.start_time = time.time()
         self.prev_btn_instance =  None
         self.prev_slider_selected = None
         self.prev_value_of_slider = None
-
+        self.last_pressed_button = None
+        
     def csvlogger(self, value_of_slider=None, slider_index=None, btn_instance=None):
         """Log the data to a csv file"""
 
         try:
-            with open(self.filename, 'a') as csvfile:
+            with open(self.filename+'.csv', 'a') as csvfile:
                 csvwriter = csv.writer(csvfile)
                 if self.logged_yet == False: # write headers & time stamp to csv file only once
                     csvwriter.writerow(self.headers)
@@ -67,33 +69,40 @@ class GuiVas(BoxLayout):
                 
                 elapsed_time = time.time() - self.start_time
                 if(config.bool_confirm_button_pressed == True):
-                    array_row = [elapsed_time, self.prev_btn_instance, self.prev_slider_selected, self.prev_value_of_slider, config.bool_confirm_button_pressed]
+                    log_array = [elapsed_time, self.prev_btn_instance, self.prev_slider_selected, self.prev_value_of_slider, config.bool_confirm_button_pressed]
                 else:
-                    array_row = [elapsed_time, btn_instance, slider_selected, value_of_slider , config.bool_confirm_button_pressed]
+                    log_array = [elapsed_time, btn_instance, slider_selected, config.button_slider_values[chr(65+slider_index)] , config.bool_confirm_button_pressed]
                     self.prev_btn_instance = btn_instance 
                     self.prev_slider_selected = slider_selected
-                    self.prev_value_of_slider = value_of_slider
-
+                    self.prev_value_of_slider = config.button_slider_values[chr(65+slider_index)]
+            
+                csvwriter.writerow(log_array)   
                 config.bool_confirm_button_pressed = False
-
-                csvwriter.writerow(array_row)   
-
         except IOError:
             print("An error occurred while trying to write to the file.")
 
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
 
-    def on_slider_value(self, instance_slider: Slider, value: float):
+    def on_slider_value(self,additional_variable, instance_slider: Slider, value: float):
         """Slider value change event method"""
         self.vas_value = value
-        print("VAS value:", self.vas_value)
 
         # Find the index of the slider that triggered the event
-        index = self.ids.slider_layout.children.index(instance_slider.parent)
+        #index = self.ids.slider_layout.children.index(instance_slider.parent)
 
+        if(additional_variable == 'A'):
+            index = 0
+        elif(additional_variable == 'B'):
+            index = 1
+        elif(additional_variable == 'C'):
+            index = 2
+        elif(additional_variable == 'D'):
+            index = 3
+        elif(additional_variable == 'E'):
+            index = 4
         # Print the VAS value and the index of the slider
-        print(f"VAS value: {self.vas_value}, Slider: {chr(65+index)}")
+        #print(f"VAS value: {self.vas_value}, Slider: {chr(65+index)}")
 
         # Update the text and position of the corresponding label
         self.labels[index].text = f"${round(value, 2)}"
@@ -102,6 +111,11 @@ class GuiVas(BoxLayout):
         
         # Set the opacity of the label to 1
         self.labels[index].opacity = 1
+
+        config.bool_slider_value_changed = True
+
+        config.button_slider_values[chr(65+index)] = self.vas_value
+        print("config.button_slider_values: ", config.button_slider_values)
 
         # Log the data to a csv file
         self.csvlogger(value, index)
@@ -118,12 +132,11 @@ class GuiVas(BoxLayout):
             torque = 4
         elif(instance_btn.text == 'E'):
             torque = 5
-    
-        """with grpc.insecure_channel(config.server_ip) as channel:
+        with grpc.insecure_channel(config.server_ip) as channel:
             stub = Message_pb2_grpc.GUIStub(channel)
             response = stub.UserButton(Message_pb2.Input(torque=torque))
 
-        print(f"You pressed the button: {instance_btn.text}")"""
+        print(f"You pressed the button: {instance_btn.text}")
         
         # Log the new torque option to a csv file
         self.csvlogger(instance_btn.text)
@@ -133,24 +146,32 @@ class GuiVas(BoxLayout):
     def confirm_button_pressed(self, instance_btn: Button):
         """Confirm button press response method"""
         print("Value of the Torque Confirmed")
-        print(f"{'CONFIRM'}")
         button = Button(text=f"{'CONFIRM'}")
         config.bool_confirm_button_pressed = True
         self.csvlogger(instance_btn.text)
+
+        config.button_order = sorted(config.button_order, key=lambda button: config.button_slider_values.get(button, -15), reverse=True)
+
+        # Clear the old button layout
+        self.ids.button_layout.clear_widgets()
+        self.create_buttons()
+        self.create_sliders()
 
     def create_buttons(self):
         """Create variable number of buttons"""
         button_colors = ['#0d9c35','#00954b','#92dc7e','#64c987','#39b48e','#089f8f','#00898a','#08737f','#215d6e','#2a4858']
         button_colors = button_colors[:self.num_torque_options]  # limit the number of buttons to the number of torque options
         self.ids.button_layout.rows =  self.num_torque_options   # set the number of columns in the grid layout
-
-        for i in range( self.num_torque_options):
-            button = Button(text=f"{chr(65+i)}") # unicode point for 'A' is 65
+        for count, i in enumerate(config.button_order):
+            # Create the button
+            button = Button(text=f"{i}") # unicode point for 'A' is 65
+            self.last_pressed_button= i
             button.bind(on_press=self.press)
-            button.background_color = button_colors[i]
+            button.background_color = button_colors[count]
             button.font_size = 64
             button.background_normal = ''
             self.ids.button_layout.add_widget(button)
+
 
     def create_sliders(self):
         """Create variable number of sliders (called only once at the beginning)"""
@@ -160,17 +181,21 @@ class GuiVas(BoxLayout):
         self.ids.slider_layout.rows = self.num_torque_options
         self.labels = []
 
-        for i in range(self.num_torque_options):
+        self.ids.slider_layout.clear_widgets()
+        for count,i in enumerate(config.button_order):
+            #for i in range(self.num_torque_options):
             # Create a BoxLayout for each slider
             box_layout = BoxLayout(orientation='horizontal')
 
             # Create the slider
-            slider = Slider(min=-15, max=50, value=0, cursor_size=(25, 25), cursor_image="pin_1.png")
-            slider.bind(value=self.on_slider_value)
-
+            slider = Slider(min=-15, max=50, value=config.button_slider_values[i], cursor_size=(25, 25), cursor_image="pin_1.png")
+            self.last_pressed_button = i
+            additional_variable = i
+            slider.bind(value=partial(self.on_slider_value, additional_variable))
             # Create the cursor label and initially set the opacity to 0
-            cursor_label = Label(text=f"${round(slider.value, 2)}", size_hint=(None, None), color=slider_colors[i],opacity=0)
+            cursor_label = Label(text=f"${round(slider.value, 2)}", size_hint=(None, None), color=slider_colors[count],opacity=1)
             self.labels.append(cursor_label)
+
 
             # Add the labels and the slider to the BoxLayout
             box_layout.add_widget(slider)
--- vas_GUI/config.py
+++ vas_GUI/config.py
@@ -1,5 +1,10 @@
 server_ip = f"{'0.0.0.0'}:" f"{'50051'}" # IP address of the Controller (RPi)
 
+#Initializing the torque mapping button order
+button_order = ['E', 'D', 'C', 'B', 'A']
+button_slider_values = {}
+for i in button_order:
+    button_slider_values[i] = 0
 
 #Initila values of the log variables
 bool_confirm_button_pressed: bool = False
