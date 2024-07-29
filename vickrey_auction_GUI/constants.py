b0c384649a6696d66ec0e58681707892a752ff76
Nundini <nundinir@umich.edu>
2024-07-29 18:32:49 -0400

    added proper ips

--- vickrey_auction_GUI/constants.py
+++ vickrey_auction_GUI/constants.py
@@ -4,7 +4,7 @@ MAX_BID = 100
 # Speed up auction time by squeeze factor FOR TESTING
 squeeze = 20
 
-server_ip = f"{'0.0.0.0'}:" f"{'50051'}"    # IP address of local machine
+server_ip = f"{'35.3.80.31'}:" f"{'50051'}"    # IP address of local machine
 client_ip = f"{'35.3.147.189'}:" f"{'50051'}"   # IP address of the tablet
 
 # Auction timings in seconds
