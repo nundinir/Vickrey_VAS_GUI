b4312fe0315f9a33a2555b6046882d2b3a799787
htchnsn <htchnsn@umich.edu>
2024-07-30 10:32:44 -0400

    IPs for network

--- vickrey_auction_GUI/constants.py
+++ vickrey_auction_GUI/constants.py
@@ -4,11 +4,11 @@ MAX_BID = 100
 # Speed up auction time by squeeze factor FOR TESTING
 squeeze = 1
 
-# SERVER_IP = f"{'35.3.150.116'}:" f"{'50051'}"    # IP address of local machine
-# CLIENT_IP = f"{'35.3.147.189'}:" f"{'50051'}"   # IP address of the tablet
+SERVER_IP = f"{'35.3.150.116'}:" f"{'50051'}"    # IP address of local machine
+CLIENT_IP = f"{'35.3.147.189'}:" f"{'50051'}"   # IP address of the tablet
 
-SERVER_IP = "localhost:50051"    # IP address of local machine
-CLIENT_IP = "[::]:50051"   # IP address of the tablet
+# SERVER_IP = "localhost:50051"    # IP address of local machine
+# CLIENT_IP = "[::]:50051"   # IP address of the tablet
 
 # Auction timings in seconds
 AUCTION_START = 0/squeeze
