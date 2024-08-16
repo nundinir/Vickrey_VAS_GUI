3c2456ef80833b91d6fc885e8047e0f01008f126
htchnsn <htchnsn@umich.edu>
2024-08-15 20:14:10 -0400

    fixes

--- vickrey_auction_GUI/exoboot_remote_control.py
+++ vickrey_auction_GUI/exoboot_remote_control.py
@@ -1,8 +1,8 @@
 import threading
 import grpc
 from concurrent import futures
-import exoboot_remote_pb2 as pb2
-import exoboot_remote_pb2_grpc as pb2_grpc
+import exoboot_remote_pb2 as pb2_r
+import exoboot_remote_pb2_grpc as pb2_grpc_r
 
 from typing import Type
 import time
@@ -12,7 +12,7 @@ from constants import PI_IP
 
 from BaseExoThread import BaseThread
 
-class ExobootCommServicer(pb2_grpc.exoboot_over_networkServicer):
+class ExobootCommServicer(pb2_grpc_r.exoboot_over_networkServicer):
     """
     Communication between anything and pi
     """
@@ -28,7 +28,7 @@ class ExobootCommServicer(pb2_grpc.exoboot_over_networkServicer):
         else:
             self.mainwrapper.pause_event.set()
 
-        return pb2.receipt_exoboot(received=True)
+        return pb2_r.receipt_exoboot(received=True)
 
     def set_quit(self, quit_msg, context):
         quit = quit_msg.quit
@@ -38,7 +38,7 @@ class ExobootCommServicer(pb2_grpc.exoboot_over_networkServicer):
         else:
             self.mainwrapper.quit_event.set()
 
-        return pb2.receipt_exoboot(received=True)
+        return pb2_r.receipt_exoboot(received=True)
 
     def set_torque(self, torque_msg, context):
         # Printing out the request from the client        
@@ -49,7 +49,7 @@ class ExobootCommServicer(pb2_grpc.exoboot_over_networkServicer):
         self.mainwrapper.gse_thread.set_peak_torque_left(peak_torque_left)
         self.mainwrapper.gse_thread.set_peak_torque_right(peak_torque_right)
 
-        return pb2.receipt_exoboot(received=True)
+        return pb2_r.receipt_exoboot(received=True)
 
 
 class RemoteThread(BaseThread):
@@ -67,7 +67,7 @@ class RemoteThread(BaseThread):
     
     def starting_server(self):
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=5))
-        pb2_grpc.add_exoboot_over_networkServicer_to_server(self.exoboot_remote_grpc, server)
+        pb2_grpc_r.add_exoboot_over_networkServicer_to_server(self.exoboot_remote_grpc, server)
         server.add_insecure_port(PI_IP)
         server.start()
         server.wait_for_termination()        
@@ -83,19 +83,19 @@ class ExobootRemoteClient:
     """
     def __init__(self):
         self.channel = grpc.insecure_channel(PI_IP)
-        self.stub = pb2_grpc.exoboot_over_networkStub(self.channel)
+        self.stub = pb2_grpc_r.exoboot_over_networkStub(self.channel)
 
     def set_pause(self, pause=False):
-        pause_msg = pb2.pause(mybool=pause)
+        pause_msg = pb2_r.pause(mybool=pause)
         receipt = self.stub.set_pause(pause_msg)
         return receipt
     
     def set_quit(self, quit=False):
-        quit_msg = pb2.quit(mybool=quit)
+        quit_msg = pb2_r.quit(mybool=quit)
         receipt = self.stub.set_quit(quit_msg)
         return receipt
 
     def set_torques(self, peak_torque_left=0, peak_torque_right=0):
-        torque_msg = pb2.torques(peak_torque_left=peak_torque_left, peak_torque_right=peak_torque_right)
+        torque_msg = pb2_r.torques(peak_torque_left=peak_torque_left, peak_torque_right=peak_torque_right)
         receipt = self.stub.set_torque(torque_msg)
         return receipt
