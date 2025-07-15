770ff4c6725c60decaa3fb7ef7e9a1ae895ed69d
htchnsn <htchnsn@umich.edu>
2025-07-15 17:26:03 -0400

    commit on this day

--- GUI_Selector/constants.py
+++ GUI_Selector/constants.py
@@ -1,7 +1,7 @@
 """IP ADDRESSES"""
 SERVER_IP = f"{'35.3.150.116'}:" f"{'50051'}"    # IP address of local machine
 LOCALHOST = "localhost:50051"    # IP address of local machine
-PI_IP = f"{'35.3.230.177'}:" f"{'50055'}"
+PI_IP = f"{'35.3.75.183'}:" f"{'50055'}"
 
 
 """BERTEC SETTINGS"""
